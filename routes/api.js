dcb757d4654180057c101bc7a515826d4d449977
samyakbambole <samyak_study@hotmail.com>
2021-04-13 17:59:05 +0530

    Redirecting /api to /api/docs

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const animals = require('../json/animals.json'); 
 
 // Routes
+router.get('/', (req,res) => {
+     res.redirect('/api/docs'); 
+}); 
+
 router.get('/animals', (req,res) => {
      res.json(animals); 
 }); 
+
 
 animals.forEach(animal => {
      router.get(`/animals/${animal.name}`, (req,res) => {
